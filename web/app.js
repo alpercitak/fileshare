10d1f06a1e567678de2e1fde462caf9d09e188f9
Alper Çıtak <citak.alper@gmail.com>
2022-05-31 11:51:02 +0300

    prettified modules

--- web/app.js
+++ web/app.js
@@ -4,12 +4,12 @@ const http = require("http");
 const path = require("path");
 
 const config = {
-    dirname: __dirname,
-    name: process.env.NAME,
-    title: process.env.NAME_READABLE,
-    http: {
-        port: process.env.PORT,
-    },
+	dirname: __dirname,
+	name: process.env.NAME,
+	title: process.env.NAME_READABLE,
+	http: {
+		port: process.env.PORT,
+	},
 };
 
 const app = express();
@@ -20,13 +20,13 @@ app.set("views", path.join(__dirname, "/views"));
 app.use(express.static("public"));
 
 app.use(async (req, res, next) => {
-    res.locals.TITLE = config.title;
-    res.locals.SOCKET_PROTOCOL = process.env.SOCKET_PROTOCOL;
-    res.locals.SOCKET_HOST = process.env.SOCKET_HOST;
-    return next();
+	res.locals.TITLE = config.title;
+	res.locals.SOCKET_PROTOCOL = process.env.SOCKET_PROTOCOL;
+	res.locals.SOCKET_HOST = process.env.SOCKET_HOST;
+	return next();
 });
 app.get("/", async (req, res) => {
-    return res.render("home.pug");
+	return res.render("home.pug");
 });
 
 httpServer.timeout = 0;
